f4f63bf341548424a8e9c1d05ac3c594309fd834
Brian Chebon <brynnlamura@gmail.com>
2023-08-15 13:49:48 +0300

    Create the Next Js app with Tailwind and TS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+// tailwind.config.js
+
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
